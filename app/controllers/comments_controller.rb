464e66f5bf6de7bf5fbf79b573942cc6e57cbe3f
paqpac <94620920+paqpac@users.noreply.github.com>
2022-02-20 15:28:54 +0900

    first commit

---
+++ app/controllers/comments_controller.rb
@@ -0,0 +1,23 @@
+class CommentsController < ApplicationController
+
+  def create
+    @comment = Comment.create(comment_params)
+    if @comment.save
+      redirect_to prototype_path(@comment.prototype)
+    else
+      @prototype = @comment.prototype
+      @comments = @prototype.comments
+      render  "prototypes/show"
+    end
+  end
+
+  def show
+    @comment =Comment.new
+    @comments = @prototype.comments.includes(:user)
+  end
+
+  private
+  def comment_params
+    params.require(:comment).permit(:content).merge(user_id: current_user.id, prototype_id: params[:prototype_id])
+  end
+end
